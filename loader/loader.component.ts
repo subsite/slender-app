81280e3e6034763d1d1a856702acef2b704bdfb2
Fredrik Welander <fredrik.welander@subsite.fi>
2016-09-16 19:31:11 +0300

    Add Google Analytics stuff

--- loader/loader.component.ts
+++ loader/loader.component.ts
@@ -4,6 +4,8 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import {LoaderService} from './loader.service';
 import { CONF } from 'local/conf';
 
+declare let ga:Function;
+
 @Component({
     template:`<div [innerHTML]="parsedMd"></div>`,
     selector: 'ss-loader',
@@ -26,13 +28,21 @@ export class LoaderComponent implements OnInit {
         console.log("loader init");
 
         this.route.params.subscribe(urlParams => {
-            //console.log(data);
             
+            
+            // send url to google analytics
+            if (CONF.gaDomains && CONF.gaDomains.indexOf(window.location.hostname) != -1) {
+                ga('send', 'pageview', location.href);
+                console.log("ga send pageview: "+location.href);
+            }
+            
+
             this.mdUrl = this.loaderService.getPageUrl(urlParams);
 
             this.loaderService.getFile(this.mdUrl)
                 .subscribe(data => {
                     this.parsedMd = this.loaderService.markUp(data);
+                    
             });
 
             
