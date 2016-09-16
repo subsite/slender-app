594e7e44a3109e39aec835801e9177bd63a7f018
fredde <fredrik.welander@subsite.fi>
2016-09-16 14:42:23 +0300

    modified:   app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule }  from '@angular/http';
-import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { InMemoryWebApiModule }     from 'angular2-in-memory-web-api';
 
@@ -12,10 +12,10 @@ import { routing } from './app.routing';
 
 import {NaviService} from './navi/navi.service';
 
-import {LoaderComponent} from './loader/loader.component';
-import {NaviComponent} from './navi/navi.component';
-import {NaviSubComponent} from './navi/navi-sub.component';
-import {FooterComponent} from './footer/footer.component';
+import { LoaderComponent } from './loader/loader.component';
+import { NaviComponent } from './navi/navi.component';
+import { NaviSubComponent } from './navi/navi-sub.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
